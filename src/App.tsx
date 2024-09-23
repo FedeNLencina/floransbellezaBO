48434ad8658e518a489385d7f0cbf944fbd32b62
Federico Lencina <federiconahuellencina@gmail.com>
2024-09-22 23:32:01 -0300

    feat: 🎸 add bottstrap cdn and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Home } from "./components/home/Home";
 
 function App() {
-  
-
   return (
-        <BrowserRouter>
+    <BrowserRouter>
       {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<></>} />
-       
+        <Route path="/" element={<Home />} />
       </Routes>
-     
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../../assets/logo.svg";
+export const Home = () => {
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center align-items-center">
+        <img className="" src={Logo}></img>
+      </div>
+      <div>Home</div>
+    </div>
+  );
+};
