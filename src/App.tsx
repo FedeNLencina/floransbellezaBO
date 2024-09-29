da3746b4cd961f612cd3b0b9c38742b18896cb89
Federico Lencina <federiconahuellencina@gmail.com>
2024-09-29 20:09:50 -0300

    feat: 🎸 add Prices component and change logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/home/Home";
+import { Prices } from "./components/prices/Prices";
+import { LogoComponent } from "./components/LogoComponent/LogoComponent";
 
 function App() {
   return (
     <BrowserRouter>
+      <LogoComponent />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/precios" element={<Home />} />
+        <Route path="/precios" element={<Prices />} />
         <Route path="/servicios" element={<Home />} />
         <Route path="/serviciosHome" element={<Home />} />
         <Route path="/imagenesGaleriaHome" element={<Home />} />
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,10 @@
-import { LogoComponent } from "./LogoComponent/LogoComponent";
+import { LogoComponent } from "../LogoComponent/LogoComponent";
 import { LinksContainer } from "./LinksContainer/LinksContainer";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="container-fluid homeContainer">
-      <LogoComponent />
       <LinksContainer />
     </div>
   );
